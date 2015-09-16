3ba37f5ce63c1cf3ab995bcc6bbd07c282929172
Danny <pacurici_daniel2004@yahoo.com>
2015-09-16 11:57:01 +0200

    create a new structure + functional tests

--- app/light_bulbs.js
+++ app/light_bulbs.js
@@ -2,12 +2,17 @@
  * Created by daniel.pacurici on 16.09.2015.
  */
 module.exports = {
-    sum: function (a, b) {
-        return a + b;
-    },
-    createBoard: createBoard
+    board: Board
+};
+
+function Board() {
+    this.table = createBoard();
+}
+
+Board.prototype.toggle = function (row, col) {
+    this.table[row][col] = 1 - this.table[row][col];
 };
 
 function createBoard() {
-    return [[1,0,0],[1,0,0],[0,0,0]];
+    return [[1, 0, 0], [1, 0, 0], [0, 0, 0]];
 }
--- spec/lightBulbsSpec.js
+++ spec/lightBulbsSpec.js
@@ -2,3 +2,24 @@
  * Created by daniel.pacurici on 16.09.2015.
  */
 var bulbs = require('../app/light_bulbs.js');
+
+describe("When value is yellow", function () {
+    var board;
+    beforeEach(function () {
+        board = new bulbs.board();
+    });
+
+    describe("we press the button", function () {
+        var row, col;
+
+        beforeEach(function () {
+            row = 1;
+            col = 2;
+            board.toggle(row, col);
+        });
+
+        it("should toggle the current value", function () {
+            expect(board.table[row][col]).toBe(1);
+        })
+    })
+});
