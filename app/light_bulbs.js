dc72984651c8a7750a380ea1a4afff01382ebf29
Danny <pacurici_daniel2004@yahoo.com>
2015-09-16 15:32:50 +0200

    add new functionality + test

--- app/light_bulbs.js
+++ app/light_bulbs.js
@@ -2,15 +2,43 @@
  * Created by daniel.pacurici on 16.09.2015.
  */
 module.exports = {
-    board: Board
+    Board: Board
 };
 
 function Board() {
     this.table = createBoard();
 }
 
+Board.prototype.shouldToggle = function (row, col) {
+    if (this.table[row][col] === 1) return;
+    this.toggle(row, col);
+};
+
 Board.prototype.toggle = function (row, col) {
     this.table[row][col] = 1 - this.table[row][col];
+
+    if (row !== -1)
+        this.table[row - 1][col] = 1 - this.table[row - 1][col];
+    if (row !== this.table.length)
+        this.table[row + 1][col] = 1 - this.table[row + 1][col];
+
+    if (col !== 0)
+        this.table[row][col - 1] = 1 - this.table[row][col - 1];
+    if (col !== this.table.length)
+        this.table[row][col + 1] = 1 - this.table[row][col + 1];
+};
+
+Board.prototype.isTurnedOff = function () {
+    var board = this.table,
+        lineIsOff;
+
+    for (var i = 0, len = board.length; i < len; i++) {
+        lineIsOff = board[i].filter(function (color) {
+            return color === 0;
+        });
+        if (lineIsOff.length > 0) return false;
+    }
+    return true;
 };
 
 function createBoard() {
--- spec/lightBulbsSpec.js
+++ spec/lightBulbsSpec.js
@@ -6,7 +6,7 @@ var bulbs = require('../app/light_bulbs.js');
 describe("When value is yellow", function () {
     var board;
     beforeEach(function () {
-        board = new bulbs.board();
+        board = new bulbs.Board();
     });
 
     describe("we press the button", function () {
@@ -14,12 +14,109 @@ describe("When value is yellow", function () {
 
         beforeEach(function () {
             row = 1;
-            col = 2;
-            board.toggle(row, col);
+            col = 1;
+            board.shouldToggle(row, col);
         });
 
         it("should toggle the current value", function () {
             expect(board.table[row][col]).toBe(1);
-        })
+        });
+
+        describe("when value isn't the last row", function () {
+            it("should toggle the bottom value as well", function () {
+                expect(board.table[row + 1][col]).toBe(1);
+            })
+        });
+
+        describe("when value isn't the first column", function () {
+            it("should toggle the top value", function () {
+                expect(board.table[row - 1][col]).toBe(1);
+            })
+        });
+
+        describe("when value isn't the first column", function () {
+            it("should toggle the left value", function () {
+                expect(board.table[row][col - 1]).toBe(0);
+            })
+        });
+
+        describe("when value isn't the last column", function () {
+            it("should toggle the right value", function () {
+                expect(board.table[row][col + 1]).toBe(1);
+            })
+        });
+
+        /* TRY TO TEST WHEN VALUE IS FIRST/LAST ROW/COL...
+         describe("when value is the last column", function () {
+         it("expect error when toggle the right value", function () {
+         row = 1;
+         col = 2;
+         board.toggle(row, col);
+         console.log(board);
+         expect(board.table[row][col + 1]).toBe(1);
+         })
+         });
+         */
     })
 });
+
+describe("When parsing the array lines", function () {
+    var board;
+    beforeEach(function () {
+        board = new bulbs.Board();
+    });
+
+    describe("When yellow value is encountered", function () {
+        beforeEach(function () {
+            spyOn(board, 'toggle');
+            board.shouldToggle(1, 1);
+        });
+
+        it('toggles', function () {
+            expect(board.toggle).toHaveBeenCalled();
+        });
+    });
+
+    describe("When black value is encountered", function () {
+        beforeEach(function () {
+            spyOn(board, 'toggle');
+            board.shouldToggle(0, 0);
+        });
+
+        it('it should not toggle', function () {
+            expect(board.toggle).not.toHaveBeenCalled();
+        });
+    });
+});
+
+describe("When array it's given", function () {
+    var board;
+    beforeEach(function () {
+        board = new bulbs.Board();
+    });
+
+
+    describe("And all bulbs are turned off", function () {
+        beforeEach(function () {
+            board.table = [[1, 1], [1, 1]]
+        });
+        it("should return true", function () {
+            expect(board.isTurnedOff()).toBe(true);
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
